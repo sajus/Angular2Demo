77e7c6631bfb33230a46fc73a3e3dd3a4bb8700c
Saju Sasidharan <sajus@cybage.com>
2017-01-03 20:37:27 +0530

    1 | adds db layer configurations

---
+++ routes/dbresources.js
@@ -0,0 +1,17 @@
+/*
+* Configuration setting for DB connection
+*/
+module.exports = {
+  database: {
+    name: 'domo_dsr',
+    host: 'root',
+    password: 'root',
+    port: 3306,
+    engine: 'INNODB',
+		charset: 'utf8',
+		collate: 'utf8_general_ci',
+		timestamps: true,
+		paranoid: false,
+		syncOnAssociation: true
+  }
+}
