94b519297dab05a7b3357121a1717f11dbc33bc0
Saju Sasidharan <sajus@cybage.com>
2017-01-03 11:51:14 +0530

    Code Beautify

--- routes/users_src.js
+++ routes/users_src.js
@@ -1,27 +1,32 @@
 var fs = require("fs"); // To require File System module to use it's functions
 
 //This function will read the JSON file and will give the output
-exports.getUsers = function (req, res) {
-   fs.readFile( "./data/datapoint.json", 'utf8', function (err, data) {
-      if (data) { res.end( data ); } // if data is present render the values
-      else
-      { res.status(404);
-        res.send("object not found"); } // It will throw an error if data is not available
-   });
+exports.getUsers = function(req, res) {
+  fs.readFile("./data/datapoint.json", 'utf8', function(err, data) {
+    if (data) {
+      res.end(data);
+    } // if data is present render the values
+    else {
+      res.status(404);
+      res.send("object not found");
+    } // It will throw an error if data is not available
+  });
 };
 
 //this function will give the user details if the requested employee ID is matched
-exports.getUserByEmpId = function (req,res) {
-   fs.readFile( "./data/datapoint.json", 'utf8', function (err, data) {
-       var users = JSON.parse( data ).userDetails; // array of objects
-       var reqParameter = req.params.empId;  // this will capture the empId requested by the user
-       for (var i=0; i<users.length; i++){
-          if (users[i].empId == reqParameter)// If the requested empId matches with the existing empID, it will display the user details
-          { var user = users[i];
-            res.end( JSON.stringify(user));}
-          else
-          { res.status(404);
-            res.send("object not found");}
-        }
-    });
+exports.getUserByEmpId = function(req, res) {
+  fs.readFile("./data/datapoint.json", 'utf8', function(err, data) {
+    var users = JSON.parse(data).userDetails; // array of objects
+    var reqParameter = req.params.empId; // this will capture the empId requested by the user
+    for (var i = 0; i < users.length; i++) {
+      // If the requested empId matches with the existing empID, it will display the user details
+      if (users[i].empId == reqParameter) {
+        var user = users[i];
+        res.end(JSON.stringify(user));
+      } else {
+        res.status(404);
+        res.send("object not found");
+      }
+    }
+  });
 };
--- server.js
+++ server.js
@@ -3,20 +3,22 @@
   AngualrCLI will do the proxy to this express server
   Don't use express here for front end serving, this will be handled by ng serve
 */
-var express   = require('express')
-  , http      = require('http')
-  , app       = express()
-  , bodyParser = require('body-parser')
-  , cookieParser = require('cookie-parser')
-  , path = require('path')
-  , users = require('./routes/users_src');
+var express = require('express'),
+  http = require('http'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  path = require('path'),
+  users = require('./routes/users_src');
 
 app.use(cookieParser());
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+})); // for parsing application/x-www-form-urlencoded
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist', 'index.html'));
 });
 
@@ -29,6 +31,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.listen(8081, function () {
+app.listen(8081, function() {
   console.log('Example listening on port 8081!');
 });
